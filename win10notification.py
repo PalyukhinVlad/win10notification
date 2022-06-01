be1b19e369896699b43849585e15abc5eab9418f
PalyukhinVlad <paluhinvlad@gmail.com>
2022-06-01 18:57:32 +0700

    change attributes

--- win10notification.py
+++ win10notification.py
@@ -2,7 +2,7 @@ import win10toast
 
 def Main():
     toast = win10toast.ToastNotifier()
-    toast.show_toast(title="Hello, World!!!", msg="Some Random Text", duration=5)
+    toast.show_toast(title="Hello, World!!!", msg="Im here", duration=10)
 
 if __name__ == "__main__":
     Main()
